2b3e59f5e08d6d16958a2059b2f229abb6c35734
sMarthz <72910166+sMarthz@users.noreply.github.com>
2021-11-27 16:42:25 +0100

    rediseño completo

--- app-planes/src/app/app.module.ts
+++ app-planes/src/app/app.module.ts
@@ -32,7 +32,7 @@ const rutas = [
   { path: 'signin', component: SigninComponent},
   { path: 'signup', component: SignupComponent},
   { path: '', component: SigninComponent},
-  { path: 'main', component: MainComponent, canActivate:[RoleGuard, AuthGuard], data: { expectedRole: 'admin' }, 
+  { path: 'main', component: MainComponent, canActivate:[], data: { expectedRole: 'admin' }, 
       children: [{ path: 'user', component: UserComponent}, { path: 'matricula', component: MatriculaComponent},]
   },
   ];
--- app-planes/src/app/header/dark-mode/dark-mode.component.ts
+++ app-planes/src/app/header/dark-mode/dark-mode.component.ts
@@ -10,6 +10,18 @@ export class DarkModeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+    const darkMode = document.getElementById('dark-mode');
+    const bloques = document.querySelectorAll('.bloque');
+    console.log(bloques);
+
+    darkMode.addEventListener('change', () => {
+      document.body.classList.toggle('dark');
+      bloques.forEach(element => {
+        element.classList.toggle('dark');
+      });
+    })
+
   }
 
 }
