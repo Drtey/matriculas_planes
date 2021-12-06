01660ecb8b0e1792c9b4d52ee61f3152b662b319
sMarthz <72910166+sMarthz@users.noreply.github.com>
2021-12-06 14:49:34 +0100

    drag and drop

--- app-planes/src/app/matricula/datos-academicos/datos-academicos.component.ts
+++ app-planes/src/app/matricula/datos-academicos/datos-academicos.component.ts
@@ -7,9 +7,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DatosAcademicosComponent implements OnInit {
 
+
+materiasOptativas = [
+  'materia1',
+  'materia2',
+  'materia3',
+  'materia4',
+  'materia5',
+  'materia6',
+  'materia7',
+  'materia8',
+]
+
+// almacenar listitems
+listItems = [];
+
+dragStartIndex
+
   constructor() { }
 
   ngOnInit(): void {
+    let draggable_list = document.getElementById('draggable-list');
+    this.createList()
+    this.listItems.forEach(element => {
+      console.log(element)
+      console.log(draggable_list)
+      draggable_list.append(element);
+    })
   }
 
+  createList() {
+    [...this.materiasOptativas]
+      .map(a => ({ value: a, sort: Math.random() }))
+      .sort((a, b) => a.sort - b.sort)
+      .map(a => a.value)
+      .forEach((materia, index:any) => {
+
+        const listItem = document.createElement('LI');
+        
+        listItem.setAttribute('data-index', index);
+
+        listItem.innerHTML = `
+          <span class="numero-materia">${index + 1}</span>
+          <div class="draggable" draggable="true">
+            <p class="nombre-materia">${materia}</p>
+            <i class="fas fa-grip-lines"></i>
+          </div>
+        `;
+
+        this.listItems.push(listItem)
+      })
+  }
+  
 }
+
