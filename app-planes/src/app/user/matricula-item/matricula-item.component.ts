b2ecb5123339d4e714a6ee18af4c003aa9a6b6f1
Drtey <hamsterminini7@gmail.com>
2021-12-12 14:05:03 +0100

    update

--- app-planes/src/app/user/matricula-item/matricula-item.component.ts
+++ app-planes/src/app/user/matricula-item/matricula-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { degrees, PDFDocument, rgb, StandardFonts } from 'pdf-lib';
 
 @Component({
   selector: 'app-matricula-item',
@@ -19,4 +20,31 @@ export class MatriculaItemComponent implements OnInit {
     
   }
 
+  async modifyPdf() {
+
+    //Fetch de un PDF que existe 
+    const url = '../../../assets/pdf/1ESO_Ordinario.pdf'
+    const existingPdfBytes = await fetch(url).then(res => res.arrayBuffer())
+  
+    //Plantilla de PDF
+    const pdfDoc = await PDFDocument.load(existingPdfBytes)
+    const helveticaFont = await pdfDoc.embedFont(StandardFonts.Helvetica)
+  
+    //Coge la la primera página
+    const pages = pdfDoc.getPages()
+    const firstPage = pages[0]
+
+    //Coge el ancho y el alto de la primera página
+    const { width, height } = firstPage.getSize()
+    firstPage.drawText('This text was added with JavaScript!', {
+      x: 5,
+      y: height / 2 + 300,
+      size: 50,
+      font: helveticaFont,
+      color: rgb(0.95, 0.1, 0.1),
+      rotate: degrees(-45),
+    })
+  
+    const pdfBytes = await pdfDoc.save();
+  }
 }
